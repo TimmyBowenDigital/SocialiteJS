b00e14631aefa55f98feb05a505b82f654478d44
TimmyBowenDigital <timothybowen9@gmail.com>
2020-01-24 17:05:35 +1000

    Add files via upload

---
+++ features.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+function FeaturesPage() {
+        return (
+          <div>
+              <h1>This is the Features Page</h1>
+          </div>
+        );
+}
+
+export default FeaturesPage
---
+++ pages/messages.jsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+class Messages extends React.Component {
+    render() {
+        return (
+            <div>
+                <title>Messages</title>
+                <h1>This is the Messages screen!!!</h1>
+            </div>
+        );
+    }
+}
+
+export default Messages
